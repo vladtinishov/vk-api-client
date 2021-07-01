877ab2711ca27d3bff6a3747687b7c45af3c36ac
vladtinishov <vladislav04022001@gmail.com>
2021-07-01 15:45:43 +0600

    Читый проект

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <CreateCampaign/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CreateCampaign from './components/CreateCampaign.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CreateCampaign
   }
 }
 </script>
---
+++ src/components/CreateCampaign.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Create Campaign Page</h1>
+        <p>Chooce your variant ad</p>
+        <ul>
+            <li><button>Text & image</button></li>
+            <li><button>Big image</button></li>
+        </ul>
+    </div>
+</template>
+<script>
+export default ({
+    methods: {
+        setData(id) {
+        }
+    }
+})
+</script>
