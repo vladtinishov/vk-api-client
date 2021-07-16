9e21ba0306de9e7084b3c5372fb66b915b269d8b
vladtinishov <54472129+vladtinishov@users.noreply.github.com>
2021-07-16 12:48:28 +0600

    Delete auth.js

--- src/auth.js
+++
@@ -1,7 +0,0 @@
-export let auth_data = {
-    access_token: '636f868f65b92cf3be01819569633ef439c900822a9f6a655ae0040621d051c161d819e4161de4b217d2a',
-    app_id: '7892486',
-    cab_id: '1603941035',
-    owner_id: '344701865',
-    public_id: '205576722',
-}
